67f0c4b06e8fd77d0ec518d4bb21f3536404e729
ander <soer9428@edu.easj.dk>
2021-12-03 11:39:10 +0100

    Opsætning af Indkøbsliste i js. html er bare en prøve version, hvor designet skal rettes til.

---
+++ indkøbsliste.js
@@ -0,0 +1,113 @@
+const clearList = document.getElementById("btn__removeall")
+const text = document.getElementById("text");
+const addTaskButton = document.getElementById("btn__add");
+const saveTaskButton = document.getElementById("btn__save");
+const listBox = document.getElementById("listBox");
+const saveInd = document.getElementById("saveIndex");
+
+
+let todoArray = [];
+
+window.addEventListener('load', (event) => {
+    JSON.parse(window.localStorage.getItem('todo'));
+    displayTodo()
+});
+
+addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(todo);
+    }
+    todoArray.push(text.value);
+    text.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+});
+
+function displayTodo() {
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(todo);
+    }
+    let htmlCode = "";
+    todoArray.forEach((list, ind) => {
+    htmlCode +=
+        `<div class="d-flex justify-content-between mt-2">
+            <p class="text-black my-auto">${list}</p>
+            <div class="">
+            <button onclick='edit(${ind})' class="rounded-3 btn btn-primary"><img class="icon__size" src="img/001-shopping-basket-sort.png" alt=""></button>
+            <button onclick='displayModal()' class="rounded-3 btn btn-danger"><img class="icon__size" src="img/001-shopping-basket-sort.png" alt=""</button>
+            </div>
+        </div>`;
+    });
+    listBox.innerHTML = htmlCode;
+}
+
+let myModal = new bootstrap.Modal(document.getElementById('exampleModal2'));
+
+function displayModal () {
+    myModal.show()
+}
+
+const btnDelete = document.getElementById('btn__delete');
+
+btnDelete.addEventListener('click',() => {
+    deleteTodo()
+    })
+
+function deleteTodo(ind) {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    todoArray.splice(ind, 1);
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+}
+
+function edit(ind) {
+    saveInd.value = ind;
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    text.value = todoArray[ind];
+    addTaskButton.style.display = "none";
+    saveTaskButton.style.display = "block";
+}
+
+saveTaskButton.addEventListener("click", () => {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    let id = saveInd.value;
+    todoArray[id] = text.value;
+    addTaskButton.style.display = "block";
+    saveTaskButton.style.display = "none";
+    text.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+});
+
+function removeAll(){
+    window.localStorage.clear();
+    document.getElementById("listBox").innerHTML = "";
+    document.getElementById("welcome").innerHTML = "Liste ";
+}
+
+clearList.addEventListener("click", () => {
+    removeAll()
+})
+
+function callme(){
+    let name = document.getElementById('tbName').value;
+    localStorage.setItem('userName', name);
+}
+
+window.addEventListener('load', (event) => {
+    if(name === null) {
+        document.getElementById('welcome').innerText = 'Liste';
+    } else {
+        document.getElementById('welcome').innerText = localStorage.getItem('userName');
+    }
+});
---
+++ indkøbsliste.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Sigende titel</title>
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <link href="css/indkøbsliste.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content rounded-3">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Indtaste et navn til listen</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form onsubmit="callme()">
+                    <p>Enter your name</p>
+                    <input id="tbName" type="text" value="Liste">
+                    <button type="button" class="btn btn-danger rounded-3 border-black" data-bs-dismiss="modal">Luk</button>
+                    <button type="submit" value="submit" class="rounded-3 border-black btn btn-primary comfirm" data-bs-dismiss="modal">Ok</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Vil du slette vare?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Nej</button>
+                <button type="button" id="btn__delete" class="btn btn-primary" data-bs-dismiss="modal">Ja</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Vil du slette vare?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Nej</button>
+                <button type="button" id="btn__removeall" class="btn btn-primary" data-bs-dismiss="modal">Ja</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container bg-white">
+    <div class="">
+            <h1 class="text-center mb-4">Indkøbsliste</h1>
+            <div class="d-flex justify-content-between mt-5 mb-5">
+                <input class="rounded-3 border-light me-3 vw-100" name="text" id="text" placeholder="tilføj vare fx 6 æg">
+                <input type="hidden" id="saveIndex">
+                <button class="rounded-3 btn btn-success flex-shrink-0" id="btn__add"><img class="icon__size" src="img/001-shopping-basket-sort.png" alt=""></button>
+                <button class="rounded-3 btn btn-success" style="display: none" id="btn__save"><img class="icon__size" src="img/001-shopping-basket-sort.png" alt=""></button>
+            </div>
+
+        <div class="d-flex">
+            <h1 id="welcome"> </h1>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">#</button>
+        </div>
+
+        <div id="listBox"></div>
+    </div>
+
+    <div class="d-flex justify-content-center mt-5">
+    <button type="button" class="btn btn-primary me-3">Gem liste</button>
+    <button type="button" class="btn btn-primary ms-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Ryd liste</button>
+    </div>
+</div>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="indkøbsliste.js"></script>
+</body>
+</html>

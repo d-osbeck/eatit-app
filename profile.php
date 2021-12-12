708bb7511cb1fe80ec7a3ff612c84fd1d7cb0e5a
ander <soer9428@edu.easj.dk>
2021-12-12 15:44:10 +0100

    - Tilføjelse af eksempler på gemte favoritter og indkøbslister.

--- profile.php
+++ profile.php
@@ -36,21 +36,21 @@
         </a>
     </div>
 
-    <div class="main__profile__content mt-4">
-        <div class="main__profile__favorites d-flex align-items-center mb-3">
+    <div class="main__favorites mt-4">
+        <div class="main__favorites__titel d-flex align-items-center mb-3">
             <h1>Favorit opskrifter</h1>
             <a href="#" class="text-black">
-                <i class="far fa-edit fa-4xl ms-2"></i>
+                <img src="img/rediger.svg" alt="rediger mine indkøbslister" style="width: 25px; height: 25px;" class="titel--icon ms-2">
             </a>
         </div>
-        <div class="items__list d-flex flex-wrap mb-4">
+        <div class="main__favorites__item items__list d-flex flex-wrap">
             <div class="d-flex flex-row mt-3 mb-3">
                 <a class="col-5 text-decoration-none text-black" href="opskrift.php?opskId=5">
                     <img src="img/bananpandekager.jpg" style="width: 100%" class="rounded-3">
                 </a>
                 <div class="col-7 ps-3 d-flex flex-column justify-content-between">
                     <div class="">
-                        <h5 class="">Bananpandekager</h5>
+                        <h5 class="mb-2">Bananpandekager</h5>
                         <div class="d-flex flex-wrap">
                             <div class="list__items py-1 px-3 m-1 align-items-center">Hurtig</div>
                             <div class="list__items py-1 px-3 m-1 align-items-center">Sukkerfri</div>
@@ -65,46 +65,81 @@
                     </div>
                 </div>
             </div>
-            <div class="items__list d-flex flex-wrap">
-                <div class="d-flex flex-row mt-3 mb-3">
-                    <a class="col-5 text-decoration-none text-black" href="opskrift.php?opskId=3">
-                        <img src="img/is.jpg" style="width: 100%" class="rounded-3">
-                    </a>
-                    <div class="col-7 ps-3 d-flex flex-column justify-content-between">
-                        <div class="">
-                            <h5 class="">Is</h5>
-                            <div class="d-flex flex-wrap">
-                                <div class="list__items py-1 px-3 m-1 align-items-center">Hurtig</div>
-                                <div class="list__items py-1 px-3 m-1 align-items-center">Sukkerfri</div>
-                                <div class="list__items py-1 px-3 m-1 align-items-center">Banan</div>
-                                <div class="list__items py-1 px-3 m-1 align-items-center">Mel</div>
-                                <div class="list__items py-1 px-3 m-1 align-items-center">Æg</div>
-                            </div>
+        </div>
+        <div class="main__favorites__item items__list d-flex flex-wrap">
+            <div class="d-flex flex-row mt-3 mb-3">
+                <a class="col-5 text-decoration-none text-black" href="opskrift.php?opskId=3">
+                    <img src="img/is.jpg" style="width: 100%" class="rounded-3">
+                </a>
+                <div class="col-7 ps-3 d-flex flex-column justify-content-between">
+                    <div class="">
+                        <h5 class="mb-2">Is</h5>
+                        <div class="d-flex flex-wrap">
+                            <div class="list__items py-1 px-3 m-1 align-items-center">Hurtig</div>
+                            <div class="list__items py-1 px-3 m-1 align-items-center">Sukkerfri</div>
+                            <div class="list__items py-1 px-3 m-1 align-items-center">Banan</div>
+                            <div class="list__items py-1 px-3 m-1 align-items-center">Mel</div>
+                            <div class="list__items py-1 px-3 m-1 align-items-center">Æg</div>
                         </div>
-                        <div class="d-flex">
-                            <p class="me-2">15 min</p>
-                            <p class="d-flex"><img src="img/stjerne_gul.svg" style="height: 20px" alt="stjerne">4/5</p>
-                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <p class="me-2">15 min</p>
+                        <p class="d-flex"><img src="img/stjerne_gul.svg" style="height: 20px" alt="stjerne">4/5</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="main__profile__lists d-flex align-items-center">
+    </div>
+
+    <div class="main__groceylist mt-4">
+        <div class="main__groceylist__titel d-flex align-items-center mb-3">
             <h1>Mine indkøbslister</h1>
             <a href="#" class="text-black">
-                <i class="far fa-edit fa-4xl ms-2"></i>
+                <img src="img/rediger.svg" alt="rediger mine indkøbslister" style="width: 25px; height: 25px;" class="titel--icon ms-2">
             </a>
+        </div>
+        <div class="main__groceylist__item items__list d-flex flex-wrap">
+            <div class="d-flex flex-row mt-3 mb-3">
+                <a class="col-5 text-decoration-none text-black" href="#">
+                    <img src="img/is.jpg" style="width: 100%" class="rounded-3">
+                </a>
+                <div class="col-7 ps-3 d-flex flex-column">
+                    <h5 class="mb-2">Ugensmadplan</h5>
+                    <div class="d-flex flex-wrap">
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Rugbrød</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Mælk</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Frugt</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Pålæg</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Morgenmad</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main__groceylist__item items__list d-flex flex-wrap">
+            <div class="d-flex flex-row mt-3 mb-3">
+                <a class="col-5 text-decoration-none text-black" href="#">
+                    <img src="img/is.jpg" style="width: 100%" class="rounded-3">
+                </a>
+                <div class="col-7 ps-3 d-flex flex-column">
+                    <h5 class="mb-2">Cheesecake</h5>
+                    <div class="d-flex flex-wrap">
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Fløde</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Flødeost</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Kiks</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Hindbær</div>
+                        <div class="list__items py-1 px-3 m-1 align-items-center">Husplast</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
 <?php $page="profil.php"?>
-
 <?php include 'nav.php'?>
 
 <script src="js/main.js"></script>
 <script src="js/showHiddenInput.js"></script>
-
 <script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
